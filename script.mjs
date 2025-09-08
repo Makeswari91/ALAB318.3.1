16b76746432340520cdca21c0dad0b63f8fc4c51
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-09-08 11:33:11 -0400

    globalerr

--- script.mjs
+++ script.mjs
@@ -8,7 +8,9 @@ const PORT = 3000;
 //Middleware
 app.use(express.json());
 //globalerror handling
-
+app.use(function (err, req, res, next) {
+  res.status(500).json({ msg: err.message });
+});
 //server listener
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`)
